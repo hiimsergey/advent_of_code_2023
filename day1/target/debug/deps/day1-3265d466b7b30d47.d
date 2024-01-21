2244bbc702dfab5730e03d5d011e0f3cca7314b3
Sergey Lavrent <goldstein.siarhei@gmail.com>
2024-01-21 19:54:04 +0100

    Initial commit

--- day1/target/debug/deps/day1-3265d466b7b30d47.d
+++
@@ -1,8 +0,0 @@
-/home/sergey/stuff/rust/advent_of_code_2023/day1/target/debug/deps/libday1-3265d466b7b30d47.rmeta: src/main.rs src/one.rs src/two.rs src/input
-
-/home/sergey/stuff/rust/advent_of_code_2023/day1/target/debug/deps/day1-3265d466b7b30d47.d: src/main.rs src/one.rs src/two.rs src/input
-
-src/main.rs:
-src/one.rs:
-src/two.rs:
-src/input:
--- day2/target/debug/deps/day2-49365a759edcfce7.d
+++
@@ -1,8 +0,0 @@
-/home/sergey/stuff/rust/advent_of_code_2023/day2/target/debug/deps/libday2-49365a759edcfce7.rmeta: src/main.rs src/one.rs src/two.rs src/input
-
-/home/sergey/stuff/rust/advent_of_code_2023/day2/target/debug/deps/day2-49365a759edcfce7.d: src/main.rs src/one.rs src/two.rs src/input
-
-src/main.rs:
-src/one.rs:
-src/two.rs:
-src/input:
