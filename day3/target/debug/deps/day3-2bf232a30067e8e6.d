c7f93fa08b944029328026dc20f5e9a77a76e231
Sergey Lavrent <goldstein.siarhei@gmail.com>
2024-01-21 19:47:13 +0100

    i give up for now...

--- day3/target/debug/deps/day3-2bf232a30067e8e6.d
+++
@@ -1,7 +0,0 @@
-/home/sergey/stuff/rust/advent_of_code_2023/day3/target/debug/deps/day3-2bf232a30067e8e6: src/main.rs src/one.rs src/input
-
-/home/sergey/stuff/rust/advent_of_code_2023/day3/target/debug/deps/day3-2bf232a30067e8e6.d: src/main.rs src/one.rs src/input
-
-src/main.rs:
-src/one.rs:
-src/input:
--- day3/target/debug/deps/day3-7cd288f65c303b3f.d
+++
@@ -1,7 +0,0 @@
-/home/sergey/stuff/rust/advent_of_code_2023/day3/target/debug/deps/libday3-7cd288f65c303b3f.rmeta: src/main.rs src/one.rs src/input
-
-/home/sergey/stuff/rust/advent_of_code_2023/day3/target/debug/deps/day3-7cd288f65c303b3f.d: src/main.rs src/one.rs src/input
-
-src/main.rs:
-src/one.rs:
-src/input:
